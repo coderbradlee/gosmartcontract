7049bab910b84a95378f41388b8157b7526b71be
lzxm160 <lzxm160@sina.com>
2018-09-05 20:50:34 +0800

    first commit

--- etclottery/calletclotteryganache_test.go
+++ etclottery/calletclotteryganache_test.go
@@ -28,26 +28,25 @@ func TestDeployCall(t *testing.T) {
 }
 
 func TestCallBuy(t *testing.T) {
+	s := NewConnecter(selfhost, callcontractAddress)
+	s.Send(userPrivateKey1, callcontractAddress, big.NewInt(5000000000000000000))
+	time.Sleep(time.Second * 10)
+	bla := s.BalanceOfEth(common.HexToAddress(callcontractAddress))
+	fmt.Println(callcontractAddress, ":", bla)
 	{
-		s := NewConnecter(selfhost, callcontractAddress)
-		s.Send(userPrivateKey1, callcontractAddress, big.NewInt(5000000000000000000))
+		ownerAuth1 := AuthAccountFromPrivateKey(userPrivateKey1)
+		s.Buy(ownerAuth1, 0)
 		time.Sleep(time.Second * 10)
-		bla := s.BalanceOfEth(common.HexToAddress(callcontractAddress))
-		fmt.Println(callcontractAddress, ":", bla)
-		{
-			ownerAuth1 := AuthAccountFromPrivateKey(userPrivateKey1)
-			s.Buy(ownerAuth1, 0)
-			time.Sleep(time.Second * 10)
-			s.Buy(ownerAuth1, 1)
-			time.Sleep(time.Second * 10)
-		}
-		{
-			ownerAuth1 := AuthAccountFromPrivateKey(userPrivateKey2)
-			s.Buy(ownerAuth1, 0)
-			time.Sleep(time.Second * 12)
-			s.Buy(ownerAuth1, 1)
-		}
-	
+		s.Buy(ownerAuth1, 1)
+		time.Sleep(time.Second * 10)
+	}
+	{
+		ownerAuth1 := AuthAccountFromPrivateKey(userPrivateKey2)
+		s.Buy(ownerAuth1, 0)
+		time.Sleep(time.Second * 12)
+		s.Buy(ownerAuth1, 1)
+	}
+
 	{
 		ret1, err := s.lottery.GetEndowmentBalance(nil)
 		if err != nil {
