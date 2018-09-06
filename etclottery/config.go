b0a7d48a38f1f7b1943a873983138f1d357d437a
lzxm160 <lzxm160@sina.com>
2018-09-06 08:39:28 +0800

    first commit

--- etclottery/config.go
+++ etclottery/config.go
@@ -11,7 +11,7 @@ import (
 var (
 	selfhost             = "http://192.168.1.10:18545"
 	contractAddress      = "0xd0c696767a2053d2f4dDF89bA894973D2b026834"
-	callcontractAddress  = "0xC3e7384C7ac46b058C73C0400dcd3Ab653fE5EdD"
+	callcontractAddress  = "0xCaA66f5EF477Ed373F909d0A501744de2301BA0a"
 	selfhostkovan        = "https://kovan.infura.io"
 	contractAddresskovan = "0xa23bb04f42be5d00be77fb912f8553b76c51889b"
 
