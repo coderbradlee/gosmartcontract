e491c018f4df998200ec29f4c3823e7b3d7814e1
lzxm160 <lzxm160@sina.com>
2018-09-11 15:35:07 +0800

    safepaymentchannel

--- safepaymentchannel/ganache_test.go
+++ safepaymentchannel/ganache_test.go
@@ -90,33 +90,45 @@ func TestBuy(t *testing.T) {
 		}
 
 	}
+}
+func TestClaim(t *testing.T) {
+	{
+		key, _ := crypto.HexToECDSA(userPrivateKey1)
+		addr := common.HexToAddress(userAddress11)
 
-	// mnemonic:="syringe height fiat cool rims loincloth weavers newt envy egotistic items rugged"
-	// for i:=0;i<100;i++{
-
-	// 	extKey,err:=hdwallet.GetPrivateKeyByPath(mnemonic,"ETH",0,0,i)
-	// 	if err!=nil{
-	// 		fmt.Println("GetPrivateKeyByPath: ", err)
-	// 		return
-	// 	}
-	// 	priKey,err:=extKey.ECPrivKey()
-	// 	if err != nil {
-	// 		return
-	// 	}
-
-	// 	privateKeyBytes := priKey.Serialize()
-	// 	private_str := hex.EncodeToString(privateKeyBytes)
-	// 	fmt.Printf("The %d ETH privateKeyBytes is %s\n", i,private_str)
-	// 	s := NewConnecter(selfhost, contractAddress)
-	// 	ownerAuth1 := AuthAccountFromPrivateKey(private_str)
-	// 	s.Buy(ownerAuth1, uint8(i%2))
-	// 	time.Sleep(time.Second*10)
+		//want to send to userAddress22 1eth nonce 10
+		msg := Keccak256([]byte(userAddress22), big.NewInt(1000000000000000000).Bytes(), big.NewInt(10).Bytes(), []byte(contractAddress))
+		sig, err := crypto.Sign(msg, key)
+		if err != nil {
+			fmt.Printf("Sign error: %s\n", err)
+		}
+		recoveredPub, err := crypto.Ecrecover(msg, sig)
+		if err != nil {
+			fmt.Printf("ECRecover error: %s\n", err)
+		}
+		pubKey, _ := crypto.UnmarshalPubkey(recoveredPub)
+		recoveredAddr := crypto.PubkeyToAddress(*pubKey)
+		if addr != recoveredAddr {
+			fmt.Printf("Address mismatch: want: %x have: %x\n", addr, recoveredAddr)
+		}
+		// fmt.Println("recoveredAddr:",recoveredAddr)
+		// // should be equal to SigToPub
+		// recoveredPub2, err := SigToPub(msg, sig)
+		// if err != nil {
+		// 	t.Errorf("ECRecover error: %s", err)
+		// }
+		// recoveredAddr2 := PubkeyToAddress(*recoveredPub2)
+		// if addr != recoveredAddr2 {
+		// 	t.Errorf("Address mismatch: want: %x have: %x", addr, recoveredAddr2)
+		// }
+	}
+	// ownerAuth1 := AuthAccountFromPrivateKey(userPrivateKey2)
+	// s := NewConnecter(selfhost, contractAddress)
+	// ret1, err := s.lottery.ClaimPayment(ownerAuth1, big.NewInt(1000000000000000000), big.NewInt(10), signature []byte)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
 	// }
-	//
-	// time.Sleep(time.Second * 10)
-	// s.Buy(ownerAuth1, 0)
-	// ownerAuth2 := AuthAccountFromPrivateKey(userPrivateKey2)
-	// s.Buy(ownerAuth2, 1)
-	// time.Sleep(time.Second * 10)
-	// s.Buy(ownerAuth2, 0)
+	// fmt.Println("ClaimPayment: ", ret1.Hash().Hex())
+
 }
