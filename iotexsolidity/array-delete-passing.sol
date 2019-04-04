c3536d7e2c1c51abe8799abfeee42264083156e5
lzxm160 <lzxm160@sina.com>
2019-04-04 13:56:23 +0800

    merge array delete and passing

--- iotexsolidity/array-delete-passing.sol
+++ iotexsolidity/array-delete-passing.sol
@@ -13,6 +13,7 @@ contract A {
 }
 
 contract Manager {
+    uint[] numbers;
     function makeA() returns (uint256) {
         uint256[] numbers;
         numbers.push(10);
@@ -21,4 +22,16 @@ contract Manager {
 
         return a.numbers(0);
     }
+    function getArray() returns (uint[]) {
+            numbers.push(100);
+            numbers.push(200);
+            numbers.push(300);
+            numbers.push(400);
+            numbers.push(500);
+
+            delete numbers[2];
+
+            // 100, 200, 0, 400, 500
+            return numbers;
+        }
 }
