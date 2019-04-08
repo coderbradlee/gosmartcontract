1085259f4eb1acb2b31e0a3c51b3b88e80c50367
limingming <limingming@wident.com>
2019-04-08 09:02:50 +0800

    go

---
+++ iotexsolidity/xx.go
@@ -0,0 +1,210 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package xx
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = abi.U256
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// MyContractABI is the input ABI used to generate the binding from.
+const MyContractABI = "[{\"constant\":false,\"inputs\":[],\"name\":\"foo\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+
+// MyContractBin is the compiled bytecode used for deploying new contracts.
+const MyContractBin = `0x608060405260008054600160a060020a031916731e14d5373e1af9cc77f0032ad2cd0fba8be5ea2e17905534801561003657600080fd5b50610139806100466000396000f3006080604052600436106100405763ffffffff7c0100000000000000000000000000000000000000000000000000000000600035041663c29855788114610045575b600080fd5b34801561005157600080fd5b5061005a61005c565b005b6000805460405173ffffffffffffffffffffffffffffffffffffffff9091169190670de0b6b3a76400009082818181858883f150506000805460405173ffffffffffffffffffffffffffffffffffffffff9091169450670de0b6b3a76400009350915081818185875af150506000805460405173ffffffffffffffffffffffffffffffffffffffff9091169350670de0b6b3a764000092509081818185875af192505050151561010b57600080fd5b5600a165627a7a723058207ddf0d51b16bab006a07afa312f19adb8c318f329bfcf0c6296a076b65e3ab560029`
+
+// DeployMyContract deploys a new Ethereum contract, binding an instance of MyContract to it.
+func DeployMyContract(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *MyContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(MyContractABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(MyContractBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &MyContract{MyContractCaller: MyContractCaller{contract: contract}, MyContractTransactor: MyContractTransactor{contract: contract}, MyContractFilterer: MyContractFilterer{contract: contract}}, nil
+}
+
+// MyContract is an auto generated Go binding around an Ethereum contract.
+type MyContract struct {
+	MyContractCaller     // Read-only binding to the contract
+	MyContractTransactor // Write-only binding to the contract
+	MyContractFilterer   // Log filterer for contract events
+}
+
+// MyContractCaller is an auto generated read-only Go binding around an Ethereum contract.
+type MyContractCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// MyContractTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type MyContractTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// MyContractFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type MyContractFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// MyContractSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type MyContractSession struct {
+	Contract     *MyContract       // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// MyContractCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type MyContractCallerSession struct {
+	Contract *MyContractCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts     // Call options to use throughout this session
+}
+
+// MyContractTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type MyContractTransactorSession struct {
+	Contract     *MyContractTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts     // Transaction auth options to use throughout this session
+}
+
+// MyContractRaw is an auto generated low-level Go binding around an Ethereum contract.
+type MyContractRaw struct {
+	Contract *MyContract // Generic contract binding to access the raw methods on
+}
+
+// MyContractCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type MyContractCallerRaw struct {
+	Contract *MyContractCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// MyContractTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type MyContractTransactorRaw struct {
+	Contract *MyContractTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewMyContract creates a new instance of MyContract, bound to a specific deployed contract.
+func NewMyContract(address common.Address, backend bind.ContractBackend) (*MyContract, error) {
+	contract, err := bindMyContract(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &MyContract{MyContractCaller: MyContractCaller{contract: contract}, MyContractTransactor: MyContractTransactor{contract: contract}, MyContractFilterer: MyContractFilterer{contract: contract}}, nil
+}
+
+// NewMyContractCaller creates a new read-only instance of MyContract, bound to a specific deployed contract.
+func NewMyContractCaller(address common.Address, caller bind.ContractCaller) (*MyContractCaller, error) {
+	contract, err := bindMyContract(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &MyContractCaller{contract: contract}, nil
+}
+
+// NewMyContractTransactor creates a new write-only instance of MyContract, bound to a specific deployed contract.
+func NewMyContractTransactor(address common.Address, transactor bind.ContractTransactor) (*MyContractTransactor, error) {
+	contract, err := bindMyContract(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &MyContractTransactor{contract: contract}, nil
+}
+
+// NewMyContractFilterer creates a new log filterer instance of MyContract, bound to a specific deployed contract.
+func NewMyContractFilterer(address common.Address, filterer bind.ContractFilterer) (*MyContractFilterer, error) {
+	contract, err := bindMyContract(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &MyContractFilterer{contract: contract}, nil
+}
+
+// bindMyContract binds a generic wrapper to an already deployed contract.
+func bindMyContract(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(MyContractABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_MyContract *MyContractRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _MyContract.Contract.MyContractCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_MyContract *MyContractRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _MyContract.Contract.MyContractTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_MyContract *MyContractRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _MyContract.Contract.MyContractTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_MyContract *MyContractCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _MyContract.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_MyContract *MyContractTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _MyContract.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_MyContract *MyContractTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _MyContract.Contract.contract.Transact(opts, method, params...)
+}
+
+// Foo is a paid mutator transaction binding the contract method 0xc2985578.
+//
+// Solidity: function foo() returns()
+func (_MyContract *MyContractTransactor) Foo(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _MyContract.contract.Transact(opts, "foo")
+}
+
+// Foo is a paid mutator transaction binding the contract method 0xc2985578.
+//
+// Solidity: function foo() returns()
+func (_MyContract *MyContractSession) Foo() (*types.Transaction, error) {
+	return _MyContract.Contract.Foo(&_MyContract.TransactOpts)
+}
+
+// Foo is a paid mutator transaction binding the contract method 0xc2985578.
+//
+// Solidity: function foo() returns()
+func (_MyContract *MyContractTransactorSession) Foo() (*types.Transaction, error) {
+	return _MyContract.Contract.Foo(&_MyContract.TransactOpts)
+}
