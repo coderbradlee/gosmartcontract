9dd182c049583892d6bbf40af6d2e0c4a73a9160
lzxm160 <lzxm160@sina.com>
2019-04-08 09:29:21 +0800

    test solidity

---
+++ iotexsolidity/connector.go
@@ -0,0 +1,118 @@
+package iotexsolidity
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+	"strings"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// var (
+// 	contractAddress = common.HexToAddress("0x84F70FEa5Ba54323C0EF85c58A47c98E1a2fe2Db")
+	
+// 	contractAddressHash = contractAddress.Hash()
+// 	selfhost        = "ws://172.16.2.12:18546"
+// )
+
+type Connecter struct {
+	ctx  context.Context
+	conn *ethclient.Client
+	contractAddress common.Address
+}
+type SpecificContract struct{
+	Connecter
+	l *MyContract
+}
+func NewConnecter(host,addr string) *SpecificContract {
+	contractAddress := common.HexToAddress(addr)
+	conn, err := ethclient.Dial(host)
+	if err != nil {
+		panic(err)
+	}
+	l, err := NewMyContract(contractAddress, conn)
+	if err != nil {
+		panic(err)
+	}
+	return &SpecificContract{
+		Connecter:  Connecter{
+			ctx:context.Background(),
+			conn: conn,
+			contractAddress:contractAddress,
+		},
+		l: l,
+	}
+}
+
+// NewConnecterWithDeploy 部署合约，并创建一个connecter
+func NewConnecterWithDeploy(host string,ownerAuth *bind.TransactOpts) *SpecificContract {
+	conn, err := ethclient.Dial(host)
+	if err != nil {
+		panic(err)
+	}
+	_, tx, l, err := DeployMyContract(ownerAuth, conn)
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	contractAddress, err := bind.WaitDeployed(ctx, conn, tx)
+	// fmt.Println("contractAddress:",contractAddress)
+
+	if err != nil {
+		panic(err)
+	}
+	return &SpecificContract{
+		Connecter:  Connecter{
+			ctx:context.Background(),
+			conn: conn,
+			contractAddress:contractAddress,
+		},
+		l: l,
+	}
+}
+
+// BlockNumber 当前块高度
+func (c *Connecter) BlockNumber() *big.Int {
+	blockNumber, err := c.conn.BlockByNumber(c.ctx, nil)
+	if err != nil {
+		log.Fatal("Get block number error", err)
+		return big.NewInt(0)
+	}
+	return blockNumber.Number()
+}
+
+// BalanceOfEth 以太币余额
+func (c *Connecter) BalanceOfEth(addr common.Address) *big.Int {
+	balance, err := c.conn.BalanceAt(c.ctx, addr, nil)
+	if err != nil {
+		log.Fatal("Get address eth balance error", err)
+		return big.NewInt(0)
+	}
+	return balance
+}
+
+// AuthAccount 解锁账户
+// 正式使用时候，此处应该限制GasPrice和GasLimit
+func AuthAccount(key, passphrase string) *bind.TransactOpts {
+	auth, err := bind.NewTransactor(strings.NewReader(key), passphrase)
+	if err != nil {
+		log.Fatalf("Auth account error: %v", err)
+		return nil
+	}
+	log.Printf("Gas price is: %v, Gas limit is: %v", auth.GasPrice, auth.GasLimit)
+	return auth
+}
+func AuthAccountFromPrivateKey(private string) *bind.TransactOpts {
+	privateKey, err := crypto.HexToECDSA(private)
+	if err != nil {
+		fmt.Println(err)
+	}
+	auth := bind.NewKeyedTransactor(privateKey)
+	
+	log.Printf("Gas price is: %v, Gas limit is: %v", auth.GasPrice, auth.GasLimit)
+	return auth
+}
---
+++ iotexsolidity/etclottery_test.go
@@ -0,0 +1,210 @@
+package iotexsolidity
+
+import (
+	// etclottery "../etclottery"
+	// "context"
+	// "crypto/ecdsa"
+	"fmt"
+
+	"testing"
+)
+
+var (
+	//metamask 0xe0e1b1e5d63e0ead6bfefcfc5a9dce543913cc15 7E76C9320595DEAA02A05DE3DE32507BC5C8B680B91E94384ADAA08CBAB0FF56
+	// 0xaa3f808a9c7bb22bc8d81dd033811b5ee2cb2207 E0EDBBB22B16FB763C9D12F63EFC735495F6716066E720F5C308A5C5B4735923
+	selfhost        = "http://127.0.0.1:18545"
+	// contractAddress = "0x01CAB100BC8878ff0b91FE8eDD9A8B3EB5b353DB"
+	// contractAddress = "0x46d26956E1e13F6Fb65427207B6e5194F359a3A0"
+	contractAddress = "0x733A9F95cE2D8EB23EdCCcFdB3A2a647aFe1F76c"
+	userAddress1="0xc416d12f3EBA9D10A1Cf21E1E6ea6509Da009ec1"
+	userAddress2="0x86993A973Cfffc4aCe686492DbA8d718e9C0eC64"
+	userPrivateKey1   = "e9348b789c81492178e4e1aedb05b0a33babd07d07efd57da9ff883a00ddbb34"
+	userPrivateKey2   = "f82110fa5ea7a7d65d0ad12a8c1576c6aec921b49343fc3ac04c7263333754eb"
+	
+)
+// func TestDeploy(t *testing.T) {
+// 	// deploy()
+// }
+// func TestWatch(t *testing.T) {
+// 	s := NewConnecter(selfhost,contractAddress)
+
+// 	s.WatchOnBuys()
+	
+// }
+func TestDeploy(t *testing.T) {
+	ownerAuth := AuthAccountFromPrivateKey(userPrivateKey1)
+	c := NewConnecterWithDeploy(selfhost,ownerAuth)
+
+	fmt.Println("Contract address is: ", c.contractAddress.String())
+	
+}
+//func TestWithdrawFee(t *testing.T){
+//	c := NewConnecter(selfhost,contractAddress)
+//	auth:=AuthAccountFromPrivateKey(userPrivateKey1)
+//	auth.GasLimit = uint64(3000000)
+//
+//	{
+//		ret1, err := c.lottery.WithdrawFee(auth, big.NewInt(10000000000000000))
+//		if err != nil {
+//			fmt.Println(err)
+//			return
+//		}
+//		fmt.Println("WithdrawFee: ", ret1.Hash().Hex())
+//	}
+//}
+//func TestGet(t *testing.T) {
+//	s := NewConnecter(selfhost,contractAddress)
+//	auth := AuthAccountFromPrivateKey(userPrivateKey1)
+//	auth.GasLimit = uint64(3000000)
+//	fmt.Println("########################################################")
+//	{
+//		ret1, err := s.lottery.GetCreator(nil)
+//		if err != nil {
+//			fmt.Println(err)
+//			return
+//		}
+//		fmt.Println("GetCreator: ", ret1.String())
+//	}
+//	fmt.Println("########################################################")
+//	{
+//		ret1, err := s.lottery.GetCurrentRoundLeft(nil)
+//		if err != nil {
+//			fmt.Println(err)
+//			return
+//		}
+//		fmt.Println("GetCurrentRoundLeft: ", ret1.String())
+//	}
+//	fmt.Println("########################################################")
+//	{
+//		ret1, err := s.lottery.GetFee(nil)
+//		if err != nil {
+//			fmt.Println(err)
+//			// return
+//		}
+//		fmt.Println("GetFee: ", ret1)
+//	}
+//	fmt.Println("########################################################")
+//	{
+//		ret1, err := s.lottery.GetEndowmentBalance(nil)
+//		if err != nil {
+//			fmt.Println(err)
+//			// return
+//		}
+//		fmt.Println("GetEndowmentBalance: ", ret1)
+//	}
+//	fmt.Println("########################################################")
+//	{
+//		ret1,err := s.lottery.GetBlock(auth)
+//		if err != nil {
+//			fmt.Println("GetBlock:",err)
+//			// return
+//		}else{
+//			fmt.Println("GetBlock:",ret1.Hash().Hex())
+//		}
+//	}
+//	// fmt.Println("########################################################")
+//	// {
+//	// 	ret1, err := s.lottery.Reinvest(auth, big.NewInt(1000000000000000000), 0)
+//	// 	if err != nil {
+//	// 		fmt.Println(err)
+//	// 		return
+//	// 	}
+//	// 	fmt.Println("Reinvest: ", ret1.Hash().Hex())
+//	// }
+//	fmt.Println("########################################################")
+//	{
+//		// GetPlayerInfoByAddress(opts *bind.CallOpts, addr common.Address) (*big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.Int, uint8, error)
+//		ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, ret9, ret10, ret11,err := s.lottery.GetPlayerInfoByAddress(nil, common.HexToAddress(userAddress1))
+//		if err != nil {
+//			fmt.Println(err)
+//			// return
+//		}
+//		fmt.Println("GetPlayerInfoByAddress: ",userAddress1)
+//		fmt.Println("currentroundIn0: ", ret1)
+//		fmt.Println("currentroundIn1: ", ret2)
+//		fmt.Println("lastRoundIn0: ", ret3)
+//		fmt.Println("lastRoundIn1: ", ret4)
+//		fmt.Println("allRoundIn: ", ret5)
+//		fmt.Println("win: ", ret6)
+//		fmt.Println("lastwin: ", ret7)
+//		fmt.Println("withdrawed: ", ret8)
+//		fmt.Println("currRoundId: ", ret9)
+//		fmt.Println("lrnd: ", ret10)
+//		fmt.Println("teamId: ", ret11)
+//	}
+//	fmt.Println("########################################################")
+//	{
+//		ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8,ret9, ret10, ret11, err := s.lottery.GetPlayerInfoByAddress(nil, common.HexToAddress(userAddress2))
+//		if err != nil {
+//			fmt.Println(err)
+//			// return
+//		}
+//		fmt.Println("GetPlayerInfoByAddress: ",userAddress2)
+//		fmt.Println("currentroundIn0: ", ret1)
+//		fmt.Println("currentroundIn1: ", ret2)
+//		fmt.Println("lastRoundIn0: ", ret3)
+//		fmt.Println("lastRoundIn1: ", ret4)
+//		fmt.Println("allRoundIn: ", ret5)
+//		fmt.Println("win: ", ret6)
+//		fmt.Println("lastwin: ", ret7)
+//		fmt.Println("withdrawed: ", ret8)
+//		fmt.Println("currRoundId: ", ret9)
+//		fmt.Println("lrnd: ", ret10)
+//		fmt.Println("teamId: ", ret11)
+//	}
+//	fmt.Println("########################################################")
+//	{
+//		// GetRoundInfo(opts *bind.CallOpts, roundId *big.Int) (*big.Int, common.Address, *big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.Int, bool, *big.Int, error)
+//		ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, ret9, ret10, err := s.lottery.GetRoundInfo(nil, big.NewInt(1))
+//		if err != nil {
+//			fmt.Println(err)
+//			// return
+//		}
+//		fmt.Println("GetRoundInfo(1)")
+//		fmt.Println("rid: ", ret1)
+//		fmt.Println("addr: ", ret2.String())
+//		fmt.Println("strt: ", ret3)
+//		fmt.Println("end: ", ret4)
+//		fmt.Println("etc: ", ret5)
+//		fmt.Println("etc0: ", ret6)
+//		fmt.Println("etc1: ", ret7)
+//		fmt.Println("pot: ", ret8)
+//		fmt.Println("ended: ", ret9)
+//		fmt.Println("winTeam: ", ret10)
+//	}
+//	fmt.Println("########################################################")
+//	{
+//		// GetCurrentInfo(opts *bind.CallOpts) (*big.Int, common.Address, *big.Int, *big.Int, *big.Int, *big.Int, *big.Int, *big.Int, bool, *big.Int, error)
+//		ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, ret9, ret10, err := s.lottery.GetCurrentInfo(nil)
+//		if err != nil {
+//			fmt.Println(err)
+//			// return
+//		}
+//		fmt.Println("GetRoundInfo")
+//		fmt.Println("rid: ", ret1)
+//		fmt.Println("addr: ", ret2.String())
+//		fmt.Println("strt: ", ret3)
+//		fmt.Println("end: ", ret4)
+//		fmt.Println("etc: ", ret5)
+//		fmt.Println("etc0: ", ret6)
+//		fmt.Println("etc1: ", ret7)
+//		fmt.Println("pot: ", ret8)
+//		fmt.Println("ended: ", ret9)
+//		fmt.Println("winTeam: ", ret10)
+//	}
+//	fmt.Println("########################################################")
+//	{
+//		// GetTotalInfo(opts *bind.CallOpts) (*big.Int, *big.Int, *big.Int, *big.Int, *big.Int, error)
+//		ret1, ret2, ret3, ret4, ret5, err := s.lottery.GetTotalInfo(nil)
+//		if err != nil {
+//			fmt.Println(err)
+//			// return
+//		}
+//		fmt.Println("totalIn: ", ret1)
+//		fmt.Println("bullTotalIn: ", ret2)
+//		fmt.Println("bearTotalIn: ", ret3)
+//		fmt.Println("bullTotalWin: ", ret4)
+//		fmt.Println("bearTotalWin: ", ret5)
+//	}
+//
+//}
