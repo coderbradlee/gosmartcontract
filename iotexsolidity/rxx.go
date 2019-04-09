39a5b6c3f7bb5ac71a677d75cb855548dcfc5bb6
limingming <limingming@wident.com>
2019-04-09 14:05:35 +0800

    x

--- iotexsolidity/rxx.go
+++ iotexsolidity/rxx.go
@@ -28,10 +28,10 @@ var (
 )
 
 // AttackerABI is the input ABI used to generate the binding from.
-const AttackerABI = "[{\"constant\":false,\"inputs\":[],\"name\":\"attack\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"name\":\"daoAddress\",\"type\":\"address\"}],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"constructor\"},{\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"fallback\"}]"
+const AttackerABI = "[{\"constant\":false,\"inputs\":[],\"name\":\"forCall\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"attack\",\"outputs\":[],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"function\"},{\"inputs\":[{\"name\":\"daoAddress\",\"type\":\"address\"}],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"constructor\"},{\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"fallback\"}]"
 
 // AttackerBin is the compiled bytecode used for deploying new contracts.
-const AttackerBin = `0x608060405260008055604051602080610272833981016040525160028054600160a060020a031916600160a060020a038316179055600a3404600155600254604080517fd0e30db00000000000000000000000000000000000000000000000000000000081529051600160a060020a039092169163d0e30db0913491600480830192600092919082900301818588803b15801561009b57600080fd5b505af11580156100af573d6000803e3d6000fd5b5050505050506101ae806100c46000396000f3006080604052600436106100405763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416639e5faafc81146100e4575b6000805460018101909155600311156100e257600254600154604080517f2e1a7d4d00000000000000000000000000000000000000000000000000000000815260048101929092525173ffffffffffffffffffffffffffffffffffffffff90921691632e1a7d4d9160248082019260009290919082900301818387803b1580156100c957600080fd5b505af11580156100dd573d6000803e3d6000fd5b505050505b005b3480156100f057600080fd5b50600254600154604080517f2e1a7d4d0000000000000000000000000000000000000000000000000000000081526004810192909252516100e29273ffffffffffffffffffffffffffffffffffffffff1691632e1a7d4d91602480830192600092919082900301818387803b15801561016857600080fd5b505af115801561017c573d6000803e3d6000fd5b505050505600a165627a7a7230582071ec3490d95a52a39fb362a1ffe72b12c88262560682d8ac308abb5246a35fb10029`
+const AttackerBin = `0x6080604052600080556040516020806101e2833981016040525160028054600160a060020a031916600160a060020a038316179055600a3404600155600254604080517fd0e30db00000000000000000000000000000000000000000000000000000000081529051600160a060020a039092169163d0e30db0913491600480830192600092919082900301818588803b15801561009b57600080fd5b505af11580156100af573d6000803e3d6000fd5b50505050505061011e806100c46000396000f30060806040526004361060485763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416634d04aa988114604a5780639e5faafc14605c575b005b348015605557600080fd5b5060486062565b60486064565b565b600254600154604080517f2e1a7d4d00000000000000000000000000000000000000000000000000000000815260048101929092525173ffffffffffffffffffffffffffffffffffffffff90921691632e1a7d4d9160248082019260009290919082900301818387803b15801560d957600080fd5b505af115801560ec573d6000803e3d6000fd5b505050505600a165627a7a72305820c9a8af876def1922341259692fa420b629355e49c2414e08a6cb15c3fab0657a0029`
 
 // DeployAttacker deploys a new Ethereum contract, binding an instance of Attacker to it.
 func DeployAttacker(auth *bind.TransactOpts, backend bind.ContractBackend, daoAddress common.Address) (common.Address, *types.Transaction, *Attacker, error) {
@@ -209,11 +209,32 @@ func (_Attacker *AttackerTransactorSession) Attack() (*types.Transaction, error)
 	return _Attacker.Contract.Attack(&_Attacker.TransactOpts)
 }
 
+// ForCall is a paid mutator transaction binding the contract method 0x4d04aa98.
+//
+// Solidity: function forCall() returns()
+func (_Attacker *AttackerTransactor) ForCall(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Attacker.contract.Transact(opts, "forCall")
+}
+
+// ForCall is a paid mutator transaction binding the contract method 0x4d04aa98.
+//
+// Solidity: function forCall() returns()
+func (_Attacker *AttackerSession) ForCall() (*types.Transaction, error) {
+	return _Attacker.Contract.ForCall(&_Attacker.TransactOpts)
+}
+
+// ForCall is a paid mutator transaction binding the contract method 0x4d04aa98.
+//
+// Solidity: function forCall() returns()
+func (_Attacker *AttackerTransactorSession) ForCall() (*types.Transaction, error) {
+	return _Attacker.Contract.ForCall(&_Attacker.TransactOpts)
+}
+
 // MiniDAOABI is the input ABI used to generate the binding from.
 const MiniDAOABI = "[{\"constant\":false,\"inputs\":[{\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"withdraw\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"deposit\",\"outputs\":[],\"payable\":true,\"stateMutability\":\"payable\",\"type\":\"function\"}]"
 
 // MiniDAOBin is the compiled bytecode used for deploying new contracts.
-const MiniDAOBin = `0x608060405234801561001057600080fd5b5060f68061001f6000396000f30060806040526004361060485763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416632e1a7d4d8114604d578063d0e30db0146064575b600080fd5b348015605857600080fd5b506062600435606a565b005b606260b3565b33600090815260208190526040902054811115608557600080fd5b60405133908290600081818185875af150503360009081526020819052604090208054939093039092555050565b3360009081526020819052604090208054340190555600a165627a7a723058205e198fd3ccc6273019393420b2c38f66a7b884fef3c3ca2f6a059b1b342e9f870029`
+const MiniDAOBin = `0x608060405234801561001057600080fd5b5061010e806100206000396000f30060806040526004361060485763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416632e1a7d4d8114604d578063d0e30db0146064575b600080fd5b348015605857600080fd5b506062600435606a565b005b606260cb565b33600090815260208190526040902054811115608557600080fd5b604051339082156108fc029083906000818181858888f1935050505015801560b1573d6000803e3d6000fd5b503360009081526020819052604090208054919091039055565b3360009081526020819052604090208054340190555600a165627a7a7230582004da340efdb6a0a157185f3dfc7fc22b2902313db009f277cb6ca5ad603929670029`
 
 // DeployMiniDAO deploys a new Ethereum contract, binding an instance of MiniDAO to it.
 func DeployMiniDAO(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *MiniDAO, error) {
